360b40f145aedc99be67af40ee329587c1390de2
Aloysius Beronque <aloysiusmatthew1@gmail.com>
2022-10-23 17:02:41 +0800

    Created title

--- Setup/src/App.js
+++ Setup/src/App.js
@@ -1,10 +1,15 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+const url = "https://course-api.com/react-tabs-project";
 function App() {
-  return <h2>tabs project setup</h2>
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>Experience</h2>
+        <div className="underline"></div>
+      </div>
+    </section>
+  );
 }
 
-export default App
+export default App;
