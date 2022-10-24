9ed03d70e9bae43bd5267286ac1e8998446130bc
Aloysius Matthew A. Beronque <aloysiusmatthew1@gmail.com>
2022-10-24 10:12:13 +0800

    Created loading

--- Setup/src/App.js
+++ Setup/src/App.js
@@ -2,9 +2,30 @@ import React, { useState, useEffect } from "react";
 import { FaAngleDoubleRight } from "react-icons/fa";
 const url = "https://course-api.com/react-tabs-project";
 function App() {
+  const [loading, setLoading] = useState(true);
   const [jobs, setJobs] = useState([]);
   const [value, setValue] = useState(0);
   // const { company } = jobs[value];
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if(loading){
+    return(
+      <section className="section loading">
+        <h1>Loading ...</h1>
+      </section>
+    )
+  }
+
   return (
     <section className="section">
       <div className="title">
